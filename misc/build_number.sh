0e672f70151275ccc6beb554bc1cb99170320a98
Aaron John Asuncion <aaron.asuncion@pointwest.com.ph>
2016-09-30 15:52:13 +0800

    Enabled build_number.sh script to update the application version upon building using gradle.

---
+++ misc/build_number.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+branch=$(git rev-parse --abbrev-ref HEAD);
+count=$(git log --oneline | wc -l | gawk '{print $1}');
+revision=$(git rev-parse --short HEAD);
+
+dash=\&#8211\;
+version=$branch$dash$count$dash$revision;
+
+echo "Version: $version";
+
+string_xml='../app/src/main/res/values/strings.xml';
+
+sed -i "s/<string name=\"build_num\">*<\/string>/<string name=\"build_num\">$version<\/string>/g" "$string_xml";
--- misc/git_version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-COMMIT_COUNT=$(git log --oneline | wc -l);
-SHA=$(git rev-parse --short HEAD);
-BRANCH=$(git rev-parse --abbrev-ref HEAD);
-
-echo "${COMMIT_COUNT}-${SHA}-${BRANCH}";
